e9feb34acdf91dde2ff8ee1225229b61a2fb611f
EmmaK <emmaculatewkamau@gmail.com>
2022-03-05 07:55:50 +0300

    Check if gender and week day are accessible from akan name function

--- scripts.js
+++ scripts.js
@@ -1,17 +1,5 @@
-/*
 
-Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
- where;
- CC - is the century digits. For example 1989 has CC = 19
- YY - is the Year digits (1989 has YY = 89)
- MM -  is the Month
- DD - is the Day of the month 
- mod - is the modulus function ( % )
-
- 0 -Sun, 1-Mon, 2-Tue ,3-Wed , 4-Thur , 5-Fri , 6-Sat
-
-*/
-
+//Function to get the day of the week from dates
 function dayOfTheWeek(){
 
     var DD = document.getElementById('day').value
@@ -24,33 +12,49 @@ function dayOfTheWeek(){
     var weekDay = parseInt(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7)
 
     if (weekDay === 0){
-        day = "Sunday";
+        day = "Sunday"
+        return day;
     }else if(weekDay === 1){
         day = "Monday";
+        return day;
     }else if(weekDay === 2){
         day = "Tuesday";
+        return day;
     }else if(weekDay === 3){
         day = "Wednesday";
+        return day;
     }else if(weekDay === 4){
         day = "Thursday";
+        return day;
     }else if(weekDay === 5){
         day = "Friday";
+        return day;
     }else if(weekDay === 6){
         day = "Saturday"
+        return day;
     }else {
         day = "What the hell just happened!!!"
+        return day;
     }
-
-    console.log(year, weekDay)
-    console.log(day)
 }
 
-function genderMale(){
-    var male = document.getElementById('male').value
-    return male
+//Function to get the gender
+
+function userGender(){
+    let male = document.getElementById('male').checked
+    if( male === true){
+        var gender = "male"
+        return gender;
+    }else{
+        var gender = "female"
+        return gender;
+    }
 }
 
-function genderFemale(){
-    var female = document.getElementById('female').value
-    return female
+//Function to retrieve the Akan name
+
+function akanName(){
+    let gender = userGender()
+    let weekDay = dayOfTheWeek()
+    console.log(gender, weekDay)
 }
