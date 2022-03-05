f8e185be32f5cebd9413c85ac403dbe34c7bfdcf
EmmaK <emmaculatewkamau@gmail.com>
2022-03-05 12:58:49 +0300

    Add month and year validator

--- scripts.js
+++ scripts.js
@@ -1,13 +1,33 @@
 //Functions to validate data
 function validateDay(){
-    let day = document.getElementById('day').value
-    let error;
+    let day = parseInt(document.getElementById('day').value)
+    let dayError;
 
-    if (isNaN(day) || day<=0 || day>31){
-        error = "Day can only be from 1 to 31"
-        document.getElementById('day-error').innerHTML = error
+    if(isNaN(day) || day<=0 || day>31){
+        dayError = "Day can only be from 1 to 31"
+        document.getElementById('day-error').innerHTML = dayError
     }
-}
+};
+
+function validateMonth(){
+    let month = parseInt(document.getElementById('month').value)
+    let monthError;
+
+    if(isNaN(month) || month<=0 || month>12){
+        monthError = "There are only 12 months in a year."
+        document.getElementById('month-error').innerHTML = monthError
+    }
+};
+
+function validateYear(){
+    let year = document.getElementById('year').value
+    let yearError
+    
+    if(year.length > 4){
+        yearError = "Invalid input!"
+        document.getElementById('year-error').innerHTML = yearError
+    }
+};
 
 //let month = document.getElementById('month').value
 //let yr = document.getElementById('year').value
@@ -15,6 +35,9 @@ function validateDay(){
 
 //Function to get the day of the week from dates
 function dayOfTheWeek(){
+    validateYear()
+    validateDay()
+    validateMonth()
     var DD = document.getElementById('day').value
     var MM = document.getElementById('month').value
     var year = document.getElementById('year').value
